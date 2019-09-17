726457952508ba25a0b76d8beda423d7ec892940
iirissetala <iirissetala@hotmail.com>
2019-09-17 15:44:19 +0300

    front mukaan

---
+++ README.md
@@ -0,0 +1,4 @@
+https://guarded-badlands-07768.herokuapp.com
+
+/api/persons - kaikki henkilöt
+/api/persons/:id - yksittäinen henkilö
---
+++ build/precache-manifest.439a365eff51dab35a6b50392a979e95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40225ad0b5cb1b28aecb7161a9b0f1c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b68f42c1d210041d96f",
+    "url": "/static/css/main.e710b32e.chunk.css"
+  },
+  {
+    "revision": "26dc41cedd24534dc3ee",
+    "url": "/static/js/2.b06bac2c.chunk.js"
+  },
+  {
+    "revision": "6b68f42c1d210041d96f",
+    "url": "/static/js/main.15a22556.chunk.js"
+  },
+  {
+    "revision": "bd15de2c81a3438b90f1",
+    "url": "/static/js/runtime~main.11bda596.js"
+  }
+]);
